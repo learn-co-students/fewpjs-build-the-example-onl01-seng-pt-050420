07f5b6fe07410ce876102f04ca42580ad98f34b0
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-12-06 14:37:37 -0500

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,43 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
 
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+const err = document.getElementById("modal")
+err.setAttribute("class", "hidden")
 
+let hearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall(e)
+   //OR: mimicServerCall("bogusUrl", {forceFailure: true})
+    .then(function(serverMessage){
+       heart.innerText = FULL_HEART
+ heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      // Basic
+      alert("Something went wrong!");
+      // or....
+      err.className = ""
+      setTimeout(() => {
+        err.className = "hidden"
+      }, 5000);
+      })
+    }
+  
+
+for ( glyph of hearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
