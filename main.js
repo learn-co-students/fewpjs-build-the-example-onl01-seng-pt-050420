5a07fc0906832d4404f95a8b712c96538ff747ae
Pwako11 <akolo@hotmail.com>
2021-02-19 23:41:18 -0500

    Done.

--- main.js
+++ main.js
@@ -1,41 +1,83 @@
 // Defining text characters for the empty and full hearts for you to use later.
+window.addEventListener("DOMContentLoaded", (event) => {
+  const modal = document.getElementById("modal")
+  const message = document.querySelector("p#modal-message")
+  modal.hidden = true; 
+
+ })
+ 
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+let glyphStates = {
+  "♡" : "♥", 
+  "♥" : "♡"
+};
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  const modal = document.getElementById("modal")
-  const message = document.querySelector("p#modal-message")
-  const posts = document.querySelectorAll("article")
+let colorStates = {
+  "red" : "",
+  "" :"red"
+};
+ 
+let articleHearts = document.querySelectorAll(".like");
 
-  modal.hidden = true; 
-  
-  for (const post of posts) {
-    const like = post.querySelector("li.like")
-    like.addEventListener("click", function(e) {
-      mimicServerCall()
-      .then(function(){
-        const heart = like.querySelector("span")
-        if (heart.innerText == EMPTY_HEART) {
-          heart.innerText = FULL_HEART
-          heart.className = "activated-heart"
-        } else {
-          heart.innerText = EMPTY_HEART
-          heart.classList.remove("activated-heart")
-        }
-      })
-      .catch((error) => {
-        
-        message.innerText = error
-        modal.classList.remove("hidden")
-        window.setTimeout(() => {
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("fakeUrl", {forceFailure: true})
+  .then(function(serverMessage){
+    heart.innerText = glyphStates[heart.innerText];
+    heart.style.color = colorStates[heart.style.color];
+  })
+  .catch(function(error){
+    document.getElementById("modal").className = ""
+    message.innerText = error
+    modal.classList.remove("hidden")
+    window.setTimeout(() => {
           modal.className = "hidden"
         }, 5000)
-      })
-    })
-  }
-})
+  });
+};
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
+
+// Your JavaScript code goes here!
+
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   const modal = document.getElementById("modal")
+//   const message = document.querySelector("p#modal-message")
+//   const posts = document.querySelectorAll("article")
+
+//   modal.hidden = true; 
+
+//   for (const post of posts) {
+//     const like = post.querySelector("li.like")
+//     like.addEventListener("click", function(e) {
+//       mimicServerCall()
+//       .then(function(){
+//         const heart = like.querySelector("span")
+//         if (heart.innerText == EMPTY_HEART) {
+//           heart.innerText = FULL_HEART
+//           heart.className = "activated-heart"
+//         } else {
+//           heart.innerText = EMPTY_HEART
+//           heart.classList.remove("activated-heart")
+//         }
+//       })
+//       .catch((error) => {
+        
+//         message.innerText = error
+//         modal.classList.remove("hidden")
+//         window.setTimeout(() => {
+//           modal.className = "hidden"
+//         }, 5000)
+//       })
+//     })
+//   }
+// })
 
 
 //------------------------------------------------------------------------------
